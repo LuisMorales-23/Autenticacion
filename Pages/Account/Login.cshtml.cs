fd417d3008e43939614e0706fd6247af184b5c07
LuisMorales-23 <dev01.Luis.morales@gmail.com>
2024-11-08 23:38:36 -0500

    Cookie

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -3,6 +3,8 @@ using Autenticacion.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Client.Platforms.Features.DesktopOs;
 using Microsoft.Identity.Client;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Autenticacion.Pages.Account
 {
@@ -23,6 +25,20 @@ namespace Autenticacion.Pages.Account
 
             if (user.Email == "correo@gmail.com" && user.Password == "12345")
             {
+                //se crea los claim, datos a almacenar en la cookie
+                var claims = new List<Claim>
+                { 
+                    new Claim (ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,user.Email),
+                };
+
+                //se asocia a los claims creados a un nombre de una cokkie 
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                //Se agrega la  identidad creada al claimsprincipal de la aplicacion 
+               ClaimsPrincipal claimsprincipal =new ClaimsPrincipal(identity);
+
+                //se registra exitosamente la autenticacion y se crea la cookie en el navegador
+                await HttpContext.SignInAsync("MyCookieAuth", claimsprincipal);
                 return RedirectToPage("/index");
             }
 
